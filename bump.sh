a8158ba724c4d77cd44ab4e581b3d93add60f936
rahmatxfauzi <madfizh@gmail.com>
2017-08-28 15:31:42 +0700

    first web commit

---
+++ bump.sh
@@ -0,0 +1,25 @@
+#!/bin /bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+#Build the project
+	
+hugo -t hyde 
+# go to public folder
+cd public
+
+# add changes to git
+git add -add
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
